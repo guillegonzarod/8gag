9347e5c272290c24ea98394e4e4c05291d0523a5
Guille <info@guillegonzarod.com>
2018-12-22 18:28:51 +0100

    Inicio del proyecto - 8gag.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -5,12 +6,13 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { HomePage, SubirPage } from '../pages/index.pages';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    SubirPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    SubirPage
   ],
   providers: [
     StatusBar,
---
+++ src/pages/index.pages.ts
@@ -0,0 +1,2 @@
+export { HomePage } from './home/home';
+export { SubirPage } from './subir/subir';
